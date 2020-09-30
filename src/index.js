1afa181629ca69285f7ac7c14a88825decf4d15a
shreeti <shreetis13@gmail.com>
2020-09-30 13:31:35 +0545

    Revised scales and fixed errors

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ let margin = {
     bottom: 50,
     left: 70
   },
-  width = 1300 - margin.left - margin.right,
-  height = 900 - margin.top - margin.bottom
+  width = 1100 - margin.left - margin.right,
+  height = 800 - margin.top - margin.bottom
 
 let svg = d3.select('.svg__boxplot')
   .append('svg')
@@ -19,14 +19,15 @@ svg = svg.append('g')
 
 d3.json('./final.json').then(function (data) {
   const allData = _.flatten(data)
-  const sortedPoints = d => d.map(g => g.points).sort(d3.ascending)
+  const sortedPoints = d => d.map(g => g.points).sort((a, b) => a - b)
   const quantileData = d3.nest()
     .key(d => d.title)
     .rollup(d => {
+      console.info(sortedPoints)
       const q1 = d3.quantile(sortedPoints(d), .25)
       const median = d3.quantile(sortedPoints(d), .5)
       const q3 = d3.quantile(sortedPoints(d), .75)
-      const IQR = q3 - q1
+      const IQR = Math.abs(q3 - q1)
       const min = q1 - 1.5 * IQR
       const max = q3 + 1.5 * IQR
       return {
@@ -41,15 +42,17 @@ d3.json('./final.json').then(function (data) {
     .entries(allData)
   console.info(quantileData)
 
+  const quantileMaximum = Math.max(...quantileData.map(d => d.value.max))
+  const quantileMinimum = Math.min(...quantileData.map(d => d.value.min))
+
   const columns = d => [...new Set(d.map(g => g.title))]
-  const rows = d => d.map(d => d.points)
+  const rows = d => d.map(d => parseInt(d.points))
 
-  const maxData = d3.max(rows(allData)) + 0.5
-  const minData = d3.min(rows(allData)) - 0.5
+  const maxValueData = Math.max(...rows(allData))
+  const minValueData = Math.min(...rows(allData))
 
-  console.info(minData)
-  console.info(maxData)
-
+  const maxData = maxValueData >= quantileMaximum ? maxValueData : quantileMaximum
+  const minData = minValueData <= quantileMinimum ? minValueData : quantileMinimum
 
   const xScale = d3.scaleBand()
     .range([0, width])
@@ -60,9 +63,13 @@ d3.json('./final.json').then(function (data) {
     .call(d3.axisBottom(xScale).tickSize(0))
     .select(".domain").remove()
 
-  const yScale = d3.scaleLinear()
+  let yScale = d3.scaleLinear()
+    .domain([0, maxData])
+    .range([0, height]);
+
+  yScale = d3.scaleLinear()
     .domain([minData, maxData])
-    .range([height, -height])
+    .range([height, 0]);
 
   svg.append('g')
     .call(d3.axisLeft(yScale).ticks(5))
@@ -140,7 +147,7 @@ d3.json('./final.json').then(function (data) {
       .duration(200)
       .style('opacity', 1)
     tooltip
-      .html("<span style='color:grey'>Points: </span>" + d.points + "<br><span style='color:grey'>" + d.year + "</span>")
+      .html(`<div class='teamInfo'><h3>${d.teamName} - ${d.title}</h3><h5>Points : ${d.points}</h5><h5>Year: ${d.year}</h5></div>`)
       .style('left', `${d3.event.pageX + 30}px`)
       .style('top', `${d3.event.pageY + 30}px`)
   }
